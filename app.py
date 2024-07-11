4ae8442eb685d95ff41de000a0e47c4782d49f3b
Addison Marcus <almarcus4@gmail.com>
2024-07-11 14:16:45 -0600

    new app

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.title("Trying this Out")
+
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
+
+st.table(chart_data)
+
+st.checkbox("I like the table")
 
 st.line_chart(chart_data)
